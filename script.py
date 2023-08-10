6b442af44b1aee3e94d89ddd8b221d1b71e1df7a
owwwl666 <zaharsalam228@gmail.com>
2023-08-10 21:34:08 +1000

    changed names functions.

--- script.py
+++ script.py
@@ -5,8 +5,8 @@ from environs import Env
 from pathlib import Path
 
 
-def returns_number_random_comic():
-    """Возвращает номер случайного комикса."""
+def upload_number_random_comic():
+    """Загружает номер случайного комикса."""
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -14,7 +14,7 @@ def returns_number_random_comic():
     return random_comic_number
 
 
-def downloads_comic(url, comic_path):
+def download_comic(url, comic_path):
     """Скачивает и сохраняет комикс."""
     response = requests.get(url)
     response.raise_for_status()
@@ -23,15 +23,15 @@ def downloads_comic(url, comic_path):
         file.write(comic_image.content)
 
 
-def returns_comments_comic(url):
-    """Возвращает комментарии к комиксу."""
+def upload_comments_comic(url):
+    """Загружает комментарии к комиксу."""
     response = requests.get(url)
     response.raise_for_status()
     comic = response.json()
     return comic['alt']
 
 
-def gets_address_upload_photo(parameters):
+def get_address_upload_photo(parameters):
     """Получает адрес для загрузки фото."""
     response = requests.get(
         f'https://api.vk.com/method/photos.getWallUploadServer', params=parameters)
@@ -39,7 +39,7 @@ def gets_address_upload_photo(parameters):
     return response.json()['response']['upload_url']
 
 
-def sends_comic_server(comic_path):
+def send_comic_server(comic_path):
     """Отправляет комикс на сервер Вконтакте.
 
     Возвращает ответ от него в виде json формата."""
@@ -49,33 +49,33 @@ def sends_comic_server(comic_path):
         files = {
             'photo': file
         }
-        url = gets_address_upload_photo(parameters)
+        url = get_address_upload_photo(parameters)
         response = requests.post(url, files=files)
         response.raise_for_status()
     return response.json()
 
 
-def saves_comic_group_album(parameters):
+def save_comic_in_group_album(parameters):
     """Сохраняет комикс в альбоме группы Вконтакте.
 
     Возвращает json с информацией о загруженном комиксе.
     """
     url = f'https://api.vk.com/method/photos.saveWallPhoto'
-    parameters |= sends_comic_server(comic_path=comic_path)
+    parameters |= send_comic_server(comic_path=comic_path)
     saving_comic_group_album = requests.post(url, params=parameters)
     saving_comic_group_album.raise_for_status()
     return saving_comic_group_album.json()
 
 
-def publishes_post_in_group(comic_number, parameters):
+def publish_post_in_group(comic_number, parameters):
     """Публикует пост на стену в группу Вконтакте."""
-    media_id = saves_comic_group_album(parameters)['response'][0]['id']
+    media_id = save_comic_in_group_album(parameters)['response'][0]['id']
     url = f'https://api.vk.com/method/wall.post'
     parameters |= {
         'owner_id': -env.int('VK_GROUP_ID'),
         'from_group': 1,
         'attachments': f'{"photo"}{env.int("VK_OWNER_ID")}_{media_id}',
-        'message': returns_comments_comic(url=f'https://xkcd.com/{comic_number}/info.0.json')
+        'message': upload_comments_comic(url=f'https://xkcd.com/{comic_number}/info.0.json')
     }
     requests.post(url, params=parameters).raise_for_status()
 
@@ -90,12 +90,12 @@ if __name__ == '__main__':
         'v': env('VK_API_VERSION'),
     }
 
-    comic_number = returns_number_random_comic()
+    comic_number = upload_number_random_comic()
 
     comic_url = f'https://xkcd.com/{comic_number}/info.0.json'
     comic_path = f'comic_{comic_number}.png'
 
-    downloads_comic(url=comic_url, comic_path=comic_path)
-    publishes_post_in_group(comic_number=comic_number, parameters=parameters)
+    download_comic(url=comic_url, comic_path=comic_path)
+    publish_post_in_group(comic_number=comic_number, parameters=parameters)
 
     os.remove(comic_path)
